309eb2f5982ab198e27ecc2309c83a8c41a5e7e2
王其春 <qichunwang@anjuke.com>
2016-06-07 18:45:04 +0800

    add test file

---
+++ Output.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spring
+ * Date: 16-6-7
+ * Time: 下午6:43
+ */
+
+namespace spring;
+
+
+class Output {
+    public  static function my_output($var)
+    {
+        var_dump($var);
+    }
+} 
